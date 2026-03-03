a7e235b9384b8f9901e6142783b37fbd2df15978
Eric Geiger <159879940+ericgeiger1@users.noreply.github.com>
2026-03-03 18:49:45 +0000

    Added measuements

--- PeachPie.cpp
+++ PeachPie.cpp
@@ -3,7 +3,34 @@ using namespace std;
 
 int main () {
 
-    cout << "Hello World!" << endl;
+        
+    int     minDelivery,
+            maxDelivery,
+            delivery; 
+            
+            
+    int peachesAvail,
+            numPies,
+            leftoverPeaches,
+            totalSugar,
+            totalFlour,
+            totalButter,
+            totalCinnamon,
+            totalVanilla;
+
+        const int peachesPerPie = 5,
+                  sugarPerPie = 24,
+                  flourPerPie = 16,
+                  butterPerPie = 8,
+                  cinnamonPerPie = 1,
+                  vanillaPerPie = 1,
+                  saltPerPie = 1,
+                  lemonJuicePerPie = 2;
+
+        const tbspsPerCup = 16;
+        const tspsPerTbsp = 3;
+
+  
 
     return 0;
 }
